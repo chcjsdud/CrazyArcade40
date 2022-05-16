5e91da19334abcd71357452591d3f3a42c968ce4
학생 정보통신공학 주세은 <12191825@office.inha.ac.kr>
2022-05-16 15:15:11 +0900

    주세은 : colmap추가

---
+++ OriginalResources/Image/TileMap/map_camp_tile4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.3" name="map_camp_tile4" tilewidth="40" tileheight="40" tilecount="1" columns="1">
+ <image source="../Map/map_camp_tile4.png" width="40" height="40"/>
+</tileset>
