66e363f1038691d0b385ad076b4f1713504539b8
학생 정보통신공학 주세은 <12191825@office.inha.ac.kr>
2022-05-16 17:14:41 +0900

    주세은 : 블럭오브젝트 추가

--- CrazyArcade/GameEngineContents/MapGameObject.cpp
+++ CrazyArcade/GameEngineContents/MapGameObject.cpp
@@ -1,18 +1,32 @@
 #include"MapGameObject.h"
+#include <GameEngine/GameEngineRenderer.h>
 
 MapGameObject::MapGameObject()
+	:MapTile_(nullptr),
+	AllBlock_()
 {
 
 }
+
 MapGameObject::~MapGameObject()
 {
 
 }
+
 void MapGameObject::Start()
 {
-
+	CreateBlockTile();
 }
+
 void MapGameObject::Update()
 {
 
+}
+
+void MapGameObject::CreateBlockTile()
+{
+	if (GetLevel()->GetNameCopy() == "VillageLevel")
+	{
+
+	}
 }
--- CrazyArcade/GameEngineContents/MapGameObject.h
+++ CrazyArcade/GameEngineContents/MapGameObject.h
@@ -1,7 +1,7 @@
 #pragma once
+#include "ContentsEnum.h"
 #include <GameEngine/GameEngineActor.h>
 #include <GameEngine/GameEngineRendererTileMap.h>
-#include "ContentsEnum.h"
 
 class GameEngineCollision;
 class BlockTile : public Tile
@@ -29,9 +29,17 @@ public:
 	MapGameObject(MapGameObject&& _Other) noexcept = delete;
 	MapGameObject& operator=(const MapGameObject& _Other) = delete;
 	MapGameObject& operator=(MapGameObject&& _Other) noexcept = delete;
+
+	void SetMapTile(GameEngineRendererTileMap* _MapTile)
+	{
+		MapTile_ = _MapTile;
+	}
+
 protected:
 	void Start() override;
 	void Update() override;
 private:
-
+	std::vector<BlockTile*> AllBlock_;
+	GameEngineRendererTileMap* MapTile_;
+	void CreateBlockTile();
 };
