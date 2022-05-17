c8b93da2a9c05364f0e784addc8d36ac0a7d9f32
학생 정보통신공학 주세은 <12191825@office.inha.ac.kr>
2022-05-17 14:33:21 +0900

    주세은 : 몬스터 레벨 front깔기

--- CrazyArcade/GameEngineContents/BossLevel.cpp
+++ CrazyArcade/GameEngineContents/BossLevel.cpp
@@ -18,6 +18,10 @@ void BossLevel::Loading()
 	MapBackGround_ = CreateActor<MapBackGround>((int)ORDER::BACKGROUND);//Actor 만들고
 	MapBackGround_->GetRenderer()->SetImage("Boss_Back.bmp");//Actor에 이미지 세팅해주고
 	MapBackGround_->GetRenderer()->SetPivot({ 320,280 });//윈도우기준 그려줄 위치 정해주고
+
+	MapFrontBackGround_ = CreateActor<MapBackGround>((int)ORDER::BACKGROUND);//Actor 만들고
+	MapFrontBackGround_->GetRenderer()->SetImage("Boss_Front.bmp");//Actor에 이미지 세팅해주고
+	MapFrontBackGround_->GetRenderer()->SetPivot({ 320,280 });//윈도우기준 그려줄 위치 정해주고
 }
 
 void BossLevel::Update()
--- CrazyArcade/GameEngineContents/Monster2Level.cpp
+++ CrazyArcade/GameEngineContents/Monster2Level.cpp
@@ -17,6 +17,11 @@ void Monster2Level::Loading()
 	MapBackGround_ = CreateActor<MapBackGround>((int)ORDER::BACKGROUND);//Actor 만들고
 	MapBackGround_->GetRenderer()->SetImage("MonsterStage2_Back.bmp");//Actor에 이미지 세팅해주고
 	MapBackGround_->GetRenderer()->SetPivot({ 320,280 });//윈도우기준 그려줄 위치 정해주고
+
+	MapFrontBackGround_ = CreateActor<MapBackGround>((int)ORDER::BACKGROUND);//Actor 만들고
+	MapFrontBackGround_->GetRenderer()->SetImage("MonsterStage2_Front.bmp");//Actor에 이미지 세팅해주고
+	MapFrontBackGround_->GetRenderer()->SetPivot({ 320,280 });//윈도우기준 그려줄 위치 정해주고
+	
 }
 void Monster2Level::Update()
 {
