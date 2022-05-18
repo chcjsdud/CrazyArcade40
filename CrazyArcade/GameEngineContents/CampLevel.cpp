69b853c94c42ff674b7afeaae3ba858f7c559e47
Eletos <lthlovelyg@naver.com>
2022-05-18 17:29:02 +0900

    이관형 : 스태시 테스트

--- CrazyArcade/GameEngineContents/CampLevel.cpp
+++ CrazyArcade/GameEngineContents/CampLevel.cpp
@@ -22,7 +22,7 @@ void CampLevel::Loading()
 {
 	MapBackGround_ = CreateActor<MapBackGround>(static_cast<int>(ORDER::BACKGROUND));//Actor 만들고
 	MapBackGround_->GetRenderer()->SetImage("Camp_Back.bmp");//Actor에 이미지 세팅해주고
-	MapBackGround_->GetRenderer()->SetPivot({320,280});//윈도우기준 그려줄 위치 정해주고
+	MapBackGround_->GetRenderer()->SetPivot({ 320,280 });//윈도우기준 그려줄 위치 정해주고
 	MapBackGround_->MapTileMap_.TileRangeSetting(15, 13, { 40,40 });// 타일맵 만들어줌
 
 
@@ -31,11 +31,11 @@ void CampLevel::Loading()
 	MapFrontBackGround_->GetRenderer()->SetImage("Camp_Front.bmp");//Actor에 이미지 세팅해주고
 	MapFrontBackGround_->GetRenderer()->SetPivot({ 320,280 });//윈도우기준 그려줄 위치 정해주고
 
-	MapGameObject* bubble = CreateActor<MapGameObject>(static_cast<int>(ORDER::EFFECT),"Bubble");
+	MapGameObject* bubble = CreateActor<MapGameObject>(static_cast<int>(ORDER::EFFECT), "Bubble");
 	bubble->SetMapTile(&MapBackGround_->MapTileMap_);
-	//bubble->BubblePop({ 60,60 }, 1);
-	bubble->CreateBoom(1, 1);
 	bubble->BubblePop({ 60,60 }, 2);
+
+	bubble->CreateBoom({ 20,20 });
 }
 
 void CampLevel::Update()
