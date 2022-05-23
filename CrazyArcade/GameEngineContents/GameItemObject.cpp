c9e977a9bf184d62fee249bc676286072abde5ee
Eletos <lthlovelyg@naver.com>
2022-05-23 15:48:58 +0900

    이관형 : 아이템 오브젝트 업데이트2

---
+++ CrazyArcade/GameEngineContents/GameItemObject.cpp
@@ -0,0 +1,58 @@
+#include"GameItemObject.h"
+#include <GameEngineBase/GameEngineTime.h>
+#include <GameEngine/GameEngineRenderer.h>
+
+GameItemObject::GameItemObject()
+	:	MapTile_(nullptr)
+{
+
+}
+GameItemObject::~GameItemObject()
+{
+
+}
+void GameItemObject::Start()
+{
+
+}
+void GameItemObject::Update()
+{
+
+}
+
+// 플레이어가 서있는 위치의 타일이 어떤 타입의 블럭인지 알려주는 함수 return 값이 Max이면 - 아무것도 없는 타일입니다.
+void GameItemObject::CreateItem(float4 _Pos, ItemType _Type)
+{
+	//if (ItemType::Max == CheckItem(_Pos))
+	//{
+	//	return;
+	//};
+
+	// 블럭이 아이템을 가지고 있을때
+	if (_Type == (ItemType::Fluid))
+	{
+	TileIndex TileIndex_ = MapTile_->GetTileIndex(_Pos);
+	float4 TileCenterPos_ = MapTile_->GetWorldPostion(TileIndex_.X, TileIndex_.Y);
+	ItemBlockTile* Tiles_ = MapTile_->CreateTile<ItemBlockTile>(TileIndex_.X, TileIndex_.Y, "TIleBase.bmp", static_cast<int>(ORDER::MAPOBJECT));
+	Tiles_->Renderer = CreateRenderer();
+	Tiles_->Renderer->SetPivot({ TileCenterPos_.x, TileCenterPos_.y }); // 아이템 y는 90이어야한다.
+
+	Tiles_->Renderer->CreateAnimation("Fluid.bmp", "Animation", 0, 5, 0.2f, true);
+	Tiles_->Renderer->ChangeAnimation("Animation");
+	}
+}
+
+// 플레이어가 서있는 위치의 타일의 BlockType이 아이템블록이라면 , 아이템이 무슨아이템인지 체크하는 함수입니당. 위치넣어주면 아이템타입이 리턴됩니당. 
+ItemType GameItemObject::CheckItem(float4 _Pos)
+{
+	TileIndex TileIndex_ = MapTile_->GetTileIndex(_Pos);
+	ItemBlockTile* Tiles_ = MapTile_->GetTile<ItemBlockTile>(TileIndex_.X, TileIndex_.Y);
+	if (Tiles_ == nullptr)
+	{
+		return ItemType::Max;
+	}
+	else
+	{
+		return Tiles_->ItemType_;
+	}
+}
---
+++ CrazyArcade/GameEngineContents/GameItemObject.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "ContentsEnum.h"
+#include <GameEngine/GameEngineActor.h>
+#include <GameEngine/GameEngineRendererTileMap.h>
+
+class GameEngineRenderer;
+
+class ItemBlockTile : public Tile
+{
+public:
+	ItemBlockTile()
+		: BlockType_(BlockType::Max),
+		ItemType_(ItemType::Max)
+	{
+
+	}
+	~ItemBlockTile()
+	{
+		if (Renderer != nullptr)
+		{
+			Renderer->Death();
+			Renderer = nullptr;
+		}
+		BlockType_ = BlockType::Max;
+		ItemType_ = ItemType::Max;
+	}
+	GameEngineRenderer* Renderer;
+	GameEngineCollision* BlockCol;
+	float4 TilePos_;
+	TileIndex TileIndex_;
+	BlockType BlockType_;
+	ItemType ItemType_;
+};
+
+
+class GameItemObject : public GameEngineActor
+{
+public:
+	GameItemObject();
+	~GameItemObject();
+
+
+	GameItemObject(const GameItemObject& _Other) = delete;
+	GameItemObject(GameItemObject&& _Other) noexcept = delete;
+	GameItemObject& operator=(const GameItemObject& _Other) = delete;
+	GameItemObject& operator=(GameItemObject&& _Other) noexcept = delete;
+
+	inline void SetMapTile(GameEngineRendererTileMap* _MapTile)
+	{
+		MapTile_ = _MapTile;
+	}
+	void CreateItem(float4 _Pos, ItemType _Type);
+	ItemType CheckItem(float4 _Pos);
+protected:
+	void Start() override;
+	void Update() override;
+private:
+	GameEngineRendererTileMap* MapTile_;
+};
