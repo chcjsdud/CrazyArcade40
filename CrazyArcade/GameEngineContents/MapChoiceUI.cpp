8f9812d60295d8c570ad6abe13db927030fd31b9
asus <asus@LAPTOP-9CPJDSIR>
2022-05-23 16:33:23 +0900

    안우진: 리소스 업데이트

--- CrazyArcade/GameEngineContents/MapChoiceUI.cpp
+++ CrazyArcade/GameEngineContents/MapChoiceUI.cpp
@@ -21,63 +21,74 @@ MapChoiceUI::~MapChoiceUI()
 
 void MapChoiceUI::Start()
 {
-	SetPosition({ 625, 410 });
-	MapUIBackGround_ = CreateRenderer((int)UIType::PopUp, RenderPivot::CENTER);
-	MapUIBackGround_->SetImage("VillageMapChoice.bmp");
+	SetPosition({ 625, 420 });
+	if (GetLevel()->GetNameConstRef() == "RoomLevel")
+	{
+		MapUIBackGround_ = CreateRenderer((int)UIType::PopUp, RenderPivot::CENTER);
+		MapUIBackGround_->SetImage("VillageMapChoice.bmp");
 
-	MapChoiceButton_ = CreateRenderer((int)UIType::PopUpButton, RenderPivot::CENTER, {77.0f,14.0f});
-	MapChoiceButton_->SetImage("MapChoiceButton.bmp");
+		MapChoiceButton_ = CreateRenderer((int)UIType::PopUpButton, RenderPivot::CENTER, { 77.0f,14.0f });
+		MapChoiceButton_->SetImage("MapChoiceButton.bmp");
 
-	MapChoiceButton_->CreateAnimation("MapChoiceButton.bmp", "MapChoiceButton_Idle", 0, 0, 0.1f, false);
-	MapChoiceButton_->CreateAnimation("MapChoiceButton.bmp", "MapChoiceButton_Approach", 1, 6, 0.1f, true);
-	MapChoiceButton_->CreateAnimation("MapChoiceButton.bmp", "MapChoiceButton_Cilck", 7, 7, 0.1f, false);
-	MapChoiceButton_->ChangeAnimation("MapChoiceButton_Idle");
+		MapChoiceButton_->CreateAnimation("MapChoiceButton.bmp", "MapChoiceButton_Idle", 0, 0, 0.1f, false);
+		MapChoiceButton_->CreateAnimation("MapChoiceButton.bmp", "MapChoiceButton_Approach", 1, 6, 0.1f, true);
+		MapChoiceButton_->CreateAnimation("MapChoiceButton.bmp", "MapChoiceButton_Cilck", 7, 7, 0.1f, false);
+		MapChoiceButton_->ChangeAnimation("MapChoiceButton_Idle");
 
-	MapButton_ = CreateCollision("MapButton", { 120, 50 }, {78, 40});
+		MapButton_ = CreateCollision("MapButton", { 120, 50 }, { 78, 40 });
 
-	choicemap = GetLevel()->CreateActor<ChoiceMap>((int)UIType::PopUp);
-	choicemap->Off();
+		choicemap = GetLevel()->CreateActor<ChoiceMap>((int)UIType::PopUp);
+		choicemap->Off();
+	}
+	else
+	{
+		MapUIBackGround_ = CreateRenderer((int)UIType::PopUp, RenderPivot::CENTER);
+		MapUIBackGround_->SetImage("VillageMapChoice.bmp");
+	}
 }
 
 
 void MapChoiceUI::Update()
 {
-	if (true == MapButton_->CollisionCheck("MouseCol"))
+
+	if (GetLevel()->GetNameConstRef() == "RoomLevel")
 	{
-		MapChoiceButton_->ChangeAnimation("MapChoiceButton_Approach");
-	}
+		if (true == MapButton_->CollisionCheck("MouseCol"))
+		{
+			MapChoiceButton_->ChangeAnimation("MapChoiceButton_Approach");
+		}
 
 
-	else if (false == MapButton_->CollisionCheck("MouseCol"))
-	{
-		MapChoiceButton_->ChangeAnimation("MapChoiceButton_Idle");
-	}
+		else if (false == MapButton_->CollisionCheck("MouseCol"))
+		{
+			MapChoiceButton_->ChangeAnimation("MapChoiceButton_Idle");
+		}
 
-	if (true == MapButton_->CollisionCheck("MouseCol") &&
-		true == GameEngineInput::GetInst()->IsPress("LeftMouse"))
-	{
-		MapChoiceButton_->ChangeAnimation("MapChoiceButton_Cilck");
-	}
+		if (true == MapButton_->CollisionCheck("MouseCol") &&
+			true == GameEngineInput::GetInst()->IsPress("LeftMouse"))
+		{
+			MapChoiceButton_->ChangeAnimation("MapChoiceButton_Cilck");
+		}
 
-	if (true == MapButton_->CollisionCheck("MouseCol") &&
-		true == GameEngineInput::GetInst()->IsPress("LeftMouse"))
-	{
-		MapButtonClick();
-	}
+		if (true == MapButton_->CollisionCheck("MouseCol") &&
+			true == GameEngineInput::GetInst()->IsPress("LeftMouse"))
+		{
+			MapButtonClick();
+		}
 
-	if (choicemap->GetChoiceMap() == 1)
-	{
-		MapUIBackGround_->SetImage("VillageMapChoice.bmp");
+		if (choicemap->GetChoiceMap() == 1)
+		{
+			MapUIBackGround_->SetImage("VillageMapChoice.bmp");
+		}
+		else if (choicemap->GetChoiceMap() == 2)
+		{
+			MapUIBackGround_->SetImage("CemetoryMapChoice.bmp");
+		}
+		else if (choicemap->GetChoiceMap() == 3)
+		{
+			MapUIBackGround_->SetImage("CampMapChoice.bmp");
+		}
 	}
-	else if (choicemap->GetChoiceMap() == 2)
-	{
-		MapUIBackGround_->SetImage("CemetoryMapChoice.bmp");
-	}
-	else if (choicemap->GetChoiceMap() == 3)
-	{
-		MapUIBackGround_->SetImage("CampMapChoice.bmp");
-	}
-
 }
 
 void MapChoiceUI::MapButtonClick()
