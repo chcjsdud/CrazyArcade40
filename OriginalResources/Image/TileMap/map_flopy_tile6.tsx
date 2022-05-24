fd3c5d46d19b70f2bfca1726f175bdada2dbefd2
학생 정보통신공학 주세은 <12191825@office.inha.ac.kr>
2022-05-24 17:41:48 +0900

    주세은 : 맵 리소스 업데이트

---
+++ OriginalResources/Image/TileMap/map_flopy_tile6.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.3" name="map_flopy_tile6" tilewidth="40" tileheight="40" tilecount="64" columns="16">
+ <image source="../Map/map_flopy_tile6.png" width="640" height="160"/>
+</tileset>
