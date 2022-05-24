bd6f96fe43c7a17a10777b8a995129374acc7e4c
Eletos <lthlovelyg@naver.com>
2022-05-24 20:30:24 +0900

    이관형 : 맵 오브젝트 이미지 편집, 캐릭터 이미지 형식 변환및 밑작업

---
+++ CrazyArcade/Resources/PlayLevel/TileMap/Camp/map_camp_tile2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.3" name="map_camp_tile2" tilewidth="40" tileheight="40" tilecount="13" columns="13">
+ <image source="../../../../../OriginalResources/Image/Map/map_camp_tile2.png" trans="ff00ff" width="520" height="40"/>
+</tileset>
