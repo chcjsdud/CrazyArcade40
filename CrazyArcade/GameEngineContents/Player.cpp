081a9b4059f29710a829639b0f0a8f01d9e2cd23
MunJongYun <answhddbs413@gmail.com>
2022-06-15 07:08:39 +0900

    문종윤 : 2p 오류수정

--- CrazyArcade/GameEngineContents/Player.cpp
+++ CrazyArcade/GameEngineContents/Player.cpp
@@ -405,7 +405,7 @@ void Player::CollisionCheck()
 						Monster* ColMonster = dynamic_cast<Monster*>(ColActor->GetActor());
 						if (ColMonster != nullptr)
 						{
-							if (ColMonster->GetHP() <= 1)
+							if (ColMonster->GetHP() <= 0)
 							{
 								return;
 							}
